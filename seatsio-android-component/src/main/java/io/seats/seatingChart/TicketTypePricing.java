e8f3b4c54f6fbf89ae312d5fa44ef76944137f4e
Ben Verbeken <ben@seats.io>
2024-04-23 10:51:27 +0200

    Add support for tickettype descriptions (#94)
    * gitignored .java-version
    
    * added description field to ticket types pricing
    
    * refactoring
    
    * deleted .java-version

--- seatsio-android-component/src/main/java/io/seats/seatingChart/TicketTypePricing.java
+++ seatsio-android-component/src/main/java/io/seats/seatingChart/TicketTypePricing.java
@@ -10,15 +10,21 @@ public class TicketTypePricing {
     public String ticketType;
     @Expose
     public String label;
+    @Expose
+    public String description;
 
     public TicketTypePricing(float price, String ticketType) {
-        this.price = price;
-        this.ticketType = ticketType;
+        this(price, ticketType, null);
     }
 
     public TicketTypePricing(float price, String ticketType, String label) {
+        this(price, ticketType, label, null);
+    }
+
+    public TicketTypePricing(float price, String ticketType, String label, String description) {
         this.price = price;
         this.ticketType = ticketType;
         this.label = label;
+        this.description = description;
     }
 }
