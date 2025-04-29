8fc2ed96102531d9a9bc21a779709a842ec86b22
Steve Chaloner <schaloner@users.noreply.github.com>
2025-04-29 13:56:57 +0200

    Add new options to Colors (#128)
    The `Colors` class was behind the times with regards to what can be possibly defined for those options. This PR updates `Colors` to match what's in the UI.
    
    Co-authored-by: Steve Chaloner <steve@seats.io>

--- seatsio-android-component/src/main/java/io/seats/seatingChart/Colors.java
+++ seatsio-android-component/src/main/java/io/seats/seatingChart/Colors.java
@@ -13,6 +13,15 @@ public class Colors {
     @Expose
     public String colorTitle;
 
+    @Expose
+    public String availableObjectColor;
+
+    @Expose
+    public String selectedObjectColor;
+
+    @Expose
+    public String errorColor;
+
     public Colors setColorSelected(String colorSelected) {
         this.colorSelected = colorSelected;
         return this;
@@ -25,6 +34,21 @@ public class Colors {
 
     public Colors setColorTitle(String colorTitle) {
         this.colorTitle = colorTitle;
+        return this;
+    }
+
+    public Colors setAvailableObjectColor(String availableObjectColor) {
+        this.availableObjectColor = availableObjectColor;
+        return this;
+    }
+
+    public Colors setSelectedObjectColor(String selectedObjectColor) {
+        this.selectedObjectColor = selectedObjectColor;
+        return this;
+    }
+
+    public Colors setErrorColor(String errorColor) {
+        this.errorColor = errorColor;
         return this;
     }
 }
